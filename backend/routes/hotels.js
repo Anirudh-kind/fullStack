228696c0333804ee15007c3e2135ca99acd9e38f
anirudh <anirudh@kindtech.co.in>
2024-09-08 22:56:13 +0530

    post request for hotel

--- backend/routes/hotels.js
+++ backend/routes/hotels.js
@@ -1,18 +1,25 @@
 import express from "express";
-import Hotel from '../models/hotelModel.js'
+import Hotel from '../models/hotelModel.js';
 
 const router = express.Router();
 
-//create
-router.post('./:id?limit=5', async () => {
-    const newHotel = new Hotel(req.body)
+// Create a new hotel
+router.post('/', async (req, res) => {
+    const newHotel = new Hotel(req.body); 
+    
+    console.log(JSON.stringify(newHotel).blue);
+
     try {
+        console.log("Started post API".blue);
+
         const savedHotel = await newHotel.save();
         res.status(200).json(savedHotel);
-        console.log('edfhnsughnrdsihn'.bgMagenta)
-    } catch {
-        res.status(500).json(err.bgRed)
-    }
-})
 
-export default router
+        console.log('Hotel saved successfully'.bgMagenta);
+    } catch (err) { 
+        console.log(err.message.bgRed);
+        res.status(500).json({ error: err.message });
+    }
+});
+
+export default router;
