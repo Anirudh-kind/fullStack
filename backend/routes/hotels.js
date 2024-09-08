1402ec48e8dc90efa90898eb7feea1a448b5f46b
anirudh <anirudh@kindtech.co.in>
2024-09-08 23:33:24 +0530

    hotel api

--- backend/routes/hotels.js
+++ backend/routes/hotels.js
@@ -5,21 +5,54 @@ const router = express.Router();
 
 // Create a new hotel
 router.post('/', async (req, res) => {
-    const newHotel = new Hotel(req.body); 
-    
-    console.log(JSON.stringify(newHotel).blue);
-
+    const newHotel = new Hotel(req.body);
     try {
-        console.log("Started post API".blue);
-
         const savedHotel = await newHotel.save();
-        res.status(200).json(savedHotel);
-
-        console.log('Hotel saved successfully'.bgMagenta);
-    } catch (err) { 
-        console.log(err.message.bgRed);
+        res.status(200).json(`new hotel created as: ${savedHotel}`);
+    } catch (err) {
         res.status(500).json({ error: err.message });
     }
 });
+
+// update
+router.put('/:id', async (req, res) => {
+    try {
+        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body })
+        res.status(200).json(` hotel updated as: ${updatedHotel}`);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+//delete
+router.delete('/:id', async (req, res) => {
+    try {
+        const deletedHotel = await Hotel.findByIdAndDelete(req.params.id)
+        res.status(200).json(` hotel deleted: ${deletedHotel}`);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+})
+
+//get specific hotel
+router.get('/:id', async (req, res) => {
+    try {
+        const getHotel = await Hotel.findById(req.params.id)
+        res.status(200).json(getHotel);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+// get all hotels
+router.get('/', async (req, res) => {
+    try {
+        const allHotel = await Hotel.find();
+        res.status(200).json(allHotel)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
 
 export default router;
