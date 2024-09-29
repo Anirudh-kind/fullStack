2263bbc25ff3836d94827150f7a71f5f992b05ba
anirudh <anirudh@kindtech.co.in>
2024-09-29 18:29:00 +0530

    auth controller done

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,7 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
+import jwt from 'jsonwebtoken'
+import 'dotenv/config';
 
 export const register = async (req, res, next) => {
   try {
@@ -35,8 +37,10 @@ export const login = async (req, res, next) => {
       console.log(`password is incorrect`.bgRed)
       return next(createError(400, 'password is incorrect'))
     }
+    const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT)
 
-    res.status(200).send('user is loged in')
+    res.cookie("access_token", token, { httpOnly: true }).status(200).send('user is loged in')
+
   } catch (error) {
     console.log(`error inside login function`.bgRed)
     next(error)
--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,14 @@ import HotelsRouter from './routes/hotels.js'
 import RoomsRouter from './routes/rooms.js'
 import UsersRoute from './routes/users.js'
 import ErrorHandler from './utils/error.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const port = process.env.PORT || 1000;
 connectDb();
 
 // middlewares
+app.use(cookieParser())
 app.use(express.json()) //middleware used to parse incoming requests with JSON payloads. When a client sends data to the server in JSON format (e.g., in the body of a POST request), this middleware processes that data and makes it available on req.body within your route handlers.
 
 app.use('/api/auth', AuthRouter)
